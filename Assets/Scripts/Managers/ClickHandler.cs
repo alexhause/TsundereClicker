8ce563c5bc7ffc635d125e2aaad950cb1915ad94
alexhause <52361639+alexhause@users.noreply.github.com>
2026-09-15 18:57:42 +0300

    добавил логику автокликера

--- Assets/Scripts/Managers/ClickHandler.cs
+++ Assets/Scripts/Managers/ClickHandler.cs
@@ -14,15 +14,16 @@ public class ClickHandler : MonoBehaviour, IPointerClickHandler
     public int ClickPower { get { return clickPower; } }
 
     [SerializeField] private LevelManager levelManager;
+    [SerializeField] private bool autoClickerEnable;
+    [SerializeField] private float autoclickInterval = 1f;
 
     private int clickPower = 1;
     private int totalClick = 0;
+    private float timer;
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        totalClick += clickPower;
-        OnClick?.Invoke();
-        OnClickAdded?.Invoke(clickPower);
+        Click();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -32,10 +33,30 @@ public class ClickHandler : MonoBehaviour, IPointerClickHandler
         levelManager.OnStageComplete += LevelManager_OnStageComplete;
     }
 
+    private void Update()
+    {
+        if (autoClickerEnable)
+        {
+            timer += Time.deltaTime;
+            if (timer >= autoclickInterval)
+            {
+                timer -= autoclickInterval; // так меньше накапливается погрешность
+                Click();
+            }
+        }
+    }
+
     private void OnDestroy()
     {
         UpgradeManager.Instance.OnClickUpgrade -= UpgradeManager_OnUpgrade;
         levelManager.OnStageComplete -= LevelManager_OnStageComplete;
+    }
+
+    private void Click()
+    {
+        totalClick += clickPower;
+        OnClick?.Invoke();
+        OnClickAdded?.Invoke(clickPower);
     }
 
     private void UpgradeManager_OnUpgrade()
